a2375f8be6d170d434846b3266bbe57f82f103ff
anasayubi <anasayubi7152@gmail.com>
2018-04-03 19:42:01 +0500

    init

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+REPEAT=1
+
+while [ $REPEAT ]
+do
+   read -p "Do you want to add pman to your path? (Yes | No) " RESP
+   RESP=$(echo "$RESP" | tr '[:upper:]' '[:lower:]')
+   if [ $RESP = yes ] || [ $RESP = y ]
+   then
+      SET_PATH=1
+      unset REPEAT
+   elif [ $RESP = no ] || [ $RESP = n ]
+   then 
+      unset SET_PATH
+      unset REPEAT
+   fi
+done
+
+#echo RESP: $RESP
+#echo SET_PATH: $SET_PATH
+
+INSTALL_LOC=$HOME/bin
+
+if [ ! -d $INSTALL_LOC ]
+then
+   mkdir $INSTALL_LOC
+fi
+cp pman $INSTALL_LOC
+mkdir $INSTALL_LOC/pman-work-dir
+mkdir $INSTALL_LOC/pman-work-dir/mans
+cp main template $INSTALL_LOC/pman-work-dir/mans
+
+# if PATH does not contain then add to PATH
+#echo PATH
+#echo $PATH
+#echo ''
+if [ $SET_PATH ] && [ ! $( echo $PATH | grep "$INSTALL_LOC:" ) ] && [ ! $( echo $PATH | grep "$INSTALL_LOC:$" ) ] && [ ! $( echo $PATH | grep "$INSTALL_LOC$" ) ]
+then
+   #echo 'write to .bashrc'
+   echo "export PATH=$INSTALL_LOC:$PATH" >> $HOME/.bashrc
+fi
+
+echo 'pman has been installed on this machine.'
